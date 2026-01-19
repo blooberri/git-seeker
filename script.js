bf1cdee8bac2a9f2a7364b7e8bd88282bf91827b
Shashwat <shashwat1043@gmail.com>
2026-01-19 19:28:51 +0530

    connect to API setup, add variables

---
+++ script.js
@@ -0,0 +1,11 @@
+// the things where we will fill
+let userProfileImg = document.querySelector('.profileImg');
+let userRealName = document.querySelector('.realname');
+let userUserName = document.querySelector('.username');
+let userUserBio = document.querySelector('.userBio');
+let userUserRepo = document.querySelector('.userRepo');
+let userUserFollowing = document.querySelector('.userFollowing');
+let userUserFollowers = document.querySelector('.userFollowers');
+//the main listeners on search box, the input and the button
+let userSearchBtn = document.querySelector('.searchIdBtn');
+let userSearchTxt = document.querySelector('.searchIdTxt');
